932ac8dff6e0bfe0d3f631f427d73b3868d24a42
DYLAN ULLRICH <DYLAN@GETULLRICHORDIETRYING.COM>
2022-07-16 20:27:41 -0700

    working on http route

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,14 +1,15 @@
 'use strict';
 
 // imports
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 
-const clientId = process.env.DISCORD_CLIENTID;
-const guildId = process.env.DISCORD_GUILDID;
+const clientId = process.env.DISCORD_CLIENT_ID;
+// const guildId = process.env.DISCORD_GUILDID;
 const token = process.env.DISCORD_TOKEN;
+console.log(clientId);
 
 const commands = [
   new SlashCommandBuilder()
@@ -17,6 +18,9 @@ const commands = [
   new SlashCommandBuilder()
     .setName('pingpat')
     .setDescription('Pings Pat, the master of ROBLOX'),
+  new SlashCommandBuilder()
+    .setName('dinglenuts')
+    .setDescription('BEHOLD... The power of DINGLENUTS'),
 ].map((command) => command.toJSON());
 
 const rest = new REST({ version: '9' }).setToken(token);
